59741f2abe68a43a7c725a106b0efa211910826e
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2022-09-08 17:53:07 -0300

    c# poo

--- Aprendizado/Enum/Program.cs
+++ Aprendizado/Enum/Program.cs
@@ -3,7 +3,13 @@
 class Program{
     public static void Main(string[] args)
     {
-        profissoes profs = (profissoes)2;
+        //varrer todos os elementos de um Enum
+        foreach (var item in Enum.GetValues(typeof(profissoes)))
+        {
+            Console.WriteLine(item);
+        }
+        //valor especifico do enum
+        profissoes profs = (profissoes)0;
         Console.Write(profs);
     }
     enum profissoes{progamador,medico,dentista}
---
+++ Aprendizado/polimorfismo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Escrever{
+    public virtual void frase(){
+        Console.Write("escrevendo algo");
+    }
+}
+
+class Lapis:Escrever{
+    public override void frase(){
+        Console.Write("escrevendo algo2");
+    }
+}
+class Program{
+    public static void Main(string[] args)
+    {
+        Lapis lapis = new Lapis();
+        lapis.frase();
+    }
+}
